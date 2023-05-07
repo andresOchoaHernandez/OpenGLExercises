7d85d82a64ce23d4a6eb41e0d5623772fd6192a0
andres <ada.wong.gv@gmail.com>
2023-05-07 21:42:47 +0200

    working on texture tutorial

---
+++ Textures/main.cpp
@@ -0,0 +1,133 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <cmath>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.hpp>
+
+#include <shader.hpp>
+
+/* HANDLING INPUT */
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+/* THIS CALLBACK FUNCTION WILL BE TRIGGERED WHEN THE USER RESIZES THE WINDOW. IT WILL SET THE NEW GL VIEWPORT */
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+} 
+
+int main(int argc, char *argv[])
+{
+    glfwInit();
+
+    /* SPECIFYING OPENGL VERSION */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    
+    /* WE WANT ONLY THE CORE FUNCTIONALITIES */
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* CREATING A WINDOW */
+    GLFWwindow* window = glfwCreateWindow(800, 600, "HelloWindow",nullptr,nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    /* THE CONTEXT OF THE WINDOW WILL BE THE CONTEXT OF THIS THREAD */
+    glfwMakeContextCurrent(window);
+
+    /* INITIALIZE GLAD */
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    /* REGISTERING CALLBACK */
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    /*============================================= SETTING UP THE TRIANGLE =============================================*/
+    float vertices[] = {
+        /* VERTEX COORDS */   /* VERTEX COLORS */
+        -0.5f, -0.5f, 0.0f,   1.0f,0.0f,0.0f,
+         0.5f, -0.5f, 0.0f,   0.0f,1.0f,0.0f,
+         0.0f,  0.5f, 0.0f,   0.0f,0.0f,1.0f,
+    };
+
+    /* VERTEX ARRAY OBJECT CREATION*/
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    /* VERTEX BUFFER OBJECT CREATION */
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    /* THE GL_ARRAY_BUFFER TARGET IS NOW BOUND TO VBO */
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    /* COPY THE DATA INTO THE CURRENT ARRAY BUFFER */
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    /* POSITION ATTRIB */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    
+    /* COLOR ATTRIBUTE */
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+    /*===================================================================================================================*/
+
+    /* ===================================================== TEXTURE SET UP ============================================ */
+    /* LOADING IMAGE */
+    int width,height,nrChannels;
+    unsigned char* data = stbi_load("../resources/tex/container.jpg",&width,&height,&nrChannels,0);
+
+    stbi_image_free(data);
+
+    float textureCoordinates[] = {
+        0.0f,0.0f,
+        1.0f,0.0f,
+        0.5f,1.0f
+    };
+
+    /*===================================================================================================================*/
+
+    Shader shaders("../shaderSources/vertexShaders/colorPos.vs","../shaderSources/fragmentShaders/colorPos.fs");
+    shaders.use();
+
+    /* RENDER LOOP */
+    while(!glfwWindowShouldClose(window))
+    {
+        /* INPUTS */
+        processInput(window);
+
+        /* RENDERING COMMANDS */
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        float time = glfwGetTime();
+        float green = sin(time)/2.0f + 0.5f;
+        
+        int vertexColorLocation = glGetUniformLocation(shaders.getProgramId(),"myColorUnif");
+        glUniform4f(vertexColorLocation,0.0f,green,0.0f,1.0f);
+
+        shaders.setFloat("offset",green);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        /* EVENTS AND BUFFER SWAP */
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+
+    glfwTerminate();
+    return 0;
+}
