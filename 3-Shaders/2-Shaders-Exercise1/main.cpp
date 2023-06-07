59929657365182bb6dc2d8a2cc883a2fafe0c054
andres <ada.wong.gv@gmail.com>
2023-06-07 14:42:22 +0200

    fixed background color in Shaders tutorial

--- 3-Shaders/2-Shaders-Exercise1/main.cpp
+++ 3-Shaders/2-Shaders-Exercise1/main.cpp
@@ -93,7 +93,7 @@ int main(int argc, char *argv[])
         processInput(window);
 
         /* RENDERING COMMANDS */
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glDrawArrays(GL_TRIANGLES, 0, 3);
