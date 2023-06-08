2c8b37fce4ac7f401521ec39353528e1b9b214ca
andres <ada.wong.gv@gmail.com>
2023-06-08 13:40:34 +0200

    changed variable names

--- 7-Camera/3-Exercise2/src/camera.cpp
+++ 7-Camera/3-Exercise2/src/camera.cpp
@@ -17,24 +17,24 @@ camFar{100.0f}
 
 glm::mat4 myLookAt(glm::vec3 pos, glm::vec3 target, glm::vec3 up)
 {
-    glm::vec3 zaxis = glm::normalize(pos - target);
-    glm::vec3 xaxis = glm::normalize(glm::cross(glm::normalize(up), zaxis));
-    glm::vec3 yaxis = glm::cross(zaxis, xaxis);
+    glm::vec3 direction = glm::normalize(pos - target);
+    glm::vec3 rigthVersor = glm::normalize(glm::cross(glm::normalize(up), direction));
+    glm::vec3 upversor = glm::cross(direction, rigthVersor);
 
     glm::mat4 translation = glm::mat4(1.0f);
     translation[3][0] = -pos.x;
     translation[3][1] = -pos.y;
     translation[3][2] = -pos.z;
     glm::mat4 rotation = glm::mat4(1.0f);
-    rotation[0][0] = xaxis.x; 
-    rotation[1][0] = xaxis.y;
-    rotation[2][0] = xaxis.z;
-    rotation[0][1] = yaxis.x; 
-    rotation[1][1] = yaxis.y;
-    rotation[2][1] = yaxis.z;
-    rotation[0][2] = zaxis.x; 
-    rotation[1][2] = zaxis.y;
-    rotation[2][2] = zaxis.z; 
+    rotation[0][0] = rigthVersor.x; 
+    rotation[1][0] = rigthVersor.y;
+    rotation[2][0] = rigthVersor.z;
+    rotation[0][1] = upversor.x; 
+    rotation[1][1] = upversor.y;
+    rotation[2][1] = upversor.z;
+    rotation[0][2] = direction.x; 
+    rotation[1][2] = direction.y;
+    rotation[2][2] = direction.z; 
     
     return rotation * translation;
 }
